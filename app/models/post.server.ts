ec3118ef99ed0102d5c477fe9fc18439ed3ab53d
Ang Lee <ang.lee@gmail.com>
2022-07-17 15:28:18 -0400

    Init

---
+++ app/models/post.server.ts
@@ -0,0 +1,28 @@
+import { prisma } from "~/db.server";
+import type { Post } from "@prisma/client";
+
+export type { Post } from "@prisma/client";
+
+export async function getPosts() {
+  // return [
+  //   {
+  //     slug: "my-first-post",
+  //     title: "My First Post",
+  //   },
+  //   {
+  //     slug: "90s-mixtape",
+  //     title: "A Mixtape I Made Just For You",
+  //   },
+  // ];
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
+}
